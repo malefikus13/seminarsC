9196f7390eab229b00cba4e1536da42e194d39fb
malefikus13 <tverdokhlebov13@gmail.com>
2023-05-27 23:10:24 +0300

    lesson_2

---
+++ task 1/Program.cs
@@ -0,0 +1,19 @@
+﻿// Задача 10: Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
+// 456 -> 5
+// 782 -> 8
+// 918 -> 1
+using System;
+
+class Program
+{
+	static void Main(string[] args)
+	{
+		Console.Write("Введите трехзначное число: ");
+		string input = Console.ReadLine(); // чтение ввода пользователя
+		int number = int.Parse(input);
+
+		int secondDigit = (number / 10) % 10; // вычисляем вторую цифру 
+
+		Console.WriteLine($"Вторая цифра числа {number}: {secondDigit}");
+	}
+}
---
+++ task 3/Program.cs
@@ -0,0 +1,31 @@
+﻿// Задача 15: Напишите программу, которая принимает на вход цифру, обозначающую день недели, и проверяет, является ли этот день выходным.
+// 6 -> да
+// 7 -> да
+// 1 -> нет
+using System;
+
+class Program
+{
+	static void Main()
+	{
+		Console.Write("Введите номер дня недели: ");
+		int day = int.Parse(Console.ReadLine());
+
+		if (day == 0)
+		{
+			Console.WriteLine("Нулевого дня не существует!");
+		}
+		else if (day < 1 || day > 7)
+		{
+			Console.WriteLine("В неделе только 7 дней!");
+		}
+		else if (day == 6 || day == 7)
+		{
+			Console.WriteLine("Выходной день!");
+		}
+		else
+		{
+			Console.WriteLine("Будний день!");
+		}
+	}
+}
