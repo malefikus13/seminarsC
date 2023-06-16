043289b145b56c266054cafc80c124ba40fba84e
malefikus13 <tverdokhlebov13@gmail.com>
2023-06-16 13:45:26 +0300

    lesson 7

---
+++ task 1/Program.cs
@@ -0,0 +1,47 @@
+﻿// Задача 47. Задайте двумерный массив размером m×n, заполненный случайными вещественными числами.
+
+// m = 3, n = 4.
+// 0,5 7 -2 -0,2
+// 1 -3,3 8 -9,9
+// 8 7,8 -7,1 9
+
+using System;
+
+class Program
+{
+	static void Main(string[] args)
+	{
+		int m, n;
+
+		Console.Write("Введите количество строк массива: ");
+		m = Convert.ToInt32(Console.ReadLine());
+
+		Console.Write("Введите количество столбцов массива: ");
+		n = Convert.ToInt32(Console.ReadLine());
+
+		double[,] array = new double[m, n];
+		Random rand = new Random();
+
+		for (int i = 0; i < m; i++)
+		{
+			for (int j = 0; j < n; j++)
+			{
+				array[i, j] = rand.NextDouble();
+			}
+		}
+
+		Console.WriteLine("Случайный массив:");
+
+		for (int i = 0; i < m; i++)
+		{
+			for (int j = 0; j < n; j++)
+			{
+				Console.Write("{0}  ", array[i, j].ToString("F2"));
+			}
+
+			Console.WriteLine();
+		}
+
+		Console.ReadKey();
+	}
+}
---
+++ task 3/Program.cs
@@ -0,0 +1,65 @@
+﻿// Задача 52. Задайте двумерный массив из целых чисел. Найдите среднее арифметическое элементов в каждом столбце.
+
+// Например, задан массив:
+// 1 4 7 2
+// 5 9 2 3
+// 8 4 2 4
+// Среднее арифметическое каждого столбца: 4,6; 5,6; 3,6; 3.
+
+class Program
+{
+	static void Main(string[] args)
+	{
+		Console.Write("Введите количество строк: ");
+		int rows = int.Parse(Console.ReadLine());
+
+		Console.Write("Введите количество столбцов: ");
+		int columns = int.Parse(Console.ReadLine());
+
+		int[,] matrix = new int[rows, columns];
+
+		Console.WriteLine("Введите элементы матрицы (разделенные пробелом):");
+
+		for (int i = 0; i < rows; i++)
+		{
+			string[] elements = Console.ReadLine().Split(' ');
+			for (int j = 0; j < columns; j++)
+			{
+				matrix[i, j] = int.Parse(elements[j]);
+			}
+		}
+
+		MatrixProcessor matrixProcessor = new MatrixProcessor(matrix);
+		matrixProcessor.CalculateColumnsAverages();
+
+		Console.ReadKey();
+	}
+}
+
+
+class MatrixProcessor
+{
+	private int[,] matrix;
+
+	public MatrixProcessor(int[,] matrix)
+	{
+		this.matrix = matrix;
+	}
+
+	public void CalculateColumnsAverages()
+	{
+		int rows = matrix.GetLength(0);
+		int columns = matrix.GetLength(1);
+
+		for (int i = 0; i < columns; i++)
+		{
+			int sum = 0;
+			for (int j = 0; j < rows; j++)
+			{
+				sum += matrix[j, i];
+			}
+			double average = (double)sum / rows;
+			Console.WriteLine("Среднее значение по столбцу {0}: {1}", i + 1, average);
+		}
+	}
+}
