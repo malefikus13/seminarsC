9d698c8147980b097214148e6a71d0fa4ce34227
malefikus13 <tverdokhlebov13@gmail.com>
2023-06-20 13:59:05 +0300

    lesson_8

---
+++ task 1/Program.cs
@@ -0,0 +1,77 @@
+﻿// Задача 54: Задайте двумерный массив. Напишите программу, которая упорядочит по убыванию элементы каждой строки двумерного массива.
+// Например, задан массив:
+// 1 4 7 2
+// 5 9 2 3
+// 8 4 2 4
+// В итоге получается вот такой массив:
+// 7 4 2 1
+// 9 5 3 2
+// 8 4 4 2
+
+using System;
+
+class Program
+{
+	static void Main()
+	{
+		Console.Write("Введите количество строк: ");
+		int n = int.Parse(Console.ReadLine());
+
+		Console.Write("Введите количество столбцов: ");
+		int m = int.Parse(Console.ReadLine());
+
+		Console.WriteLine("Введите элементы массива через пробел:");
+
+		int[,] arr = new int[n, m];
+
+		for (int i = 0; i < n; i++)
+		{
+			string[] row = Console.ReadLine().Split(' ');
+
+			for (int j = 0; j < m; j++)
+			{
+				arr[i, j] = int.Parse(row[j]);
+			}
+		}
+
+		Console.WriteLine("Введенный массив:");
+
+		for (int i = 0; i < n; i++)
+		{
+			for (int j = 0; j < m; j++)
+			{
+				Console.Write("{0}\t", arr[i, j]);
+			}
+			Console.WriteLine();
+		}
+
+		Console.WriteLine("Отсортированный массив:");
+
+		for (int i = 0; i < n; i++)
+		{
+			for (int j = 0; j < m; j++)
+			{
+				for (int k = j + 1; k < m; k++)
+				{
+					if (arr[i, j] < arr[i, k])
+					{
+						int temp = arr[i, j];
+						arr[i, j] = arr[i, k];
+						arr[i, k] = temp;
+					}
+				}
+			}
+		}
+
+		for (int i = 0; i < n; i++)
+		{
+			for (int j = 0; j < m; j++)
+			{
+				Console.Write("{0}\t", arr[i, j]);
+			}
+			Console.WriteLine();
+		}
+
+		Console.ReadKey();
+	}
+}
---
+++ task 4/Program.cs
@@ -0,0 +1,67 @@
+﻿// Задача 60. ...Сформируйте трёхмерный массив из неповторяющихся двузначных чисел. Напишите программу, которая будет построчно выводить массив, добавляя индексы каждого элемента.
+// Массив размером 2 x 2 x 2
+// 66(0,0,0) 25(0,1,0)
+// 34(1,0,0) 41(1,1,0)
+// 27(0,0,1) 90(0,1,1)
+// 26(1,0,1) 55(1,1,1)
+
+using System;
+
+namespace ThreeDimensionalArray
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			int[,,] array = new int[2, 2, 2];
+			for (int i = 0; i < 2; i++)
+			{
+				for (int j = 0; j < 2; j++)
+				{
+					for (int k = 0; k < 2; k++)
+					{
+						Console.Write($"Введите уникальный двузначный номер для индекса ({i},{j},{k}): ");
+						bool isInputValid = int.TryParse(Console.ReadLine(), out int input) && input >= 10 && input <= 99 && !ArrayContainsValue(array, input);
+						while (!isInputValid)
+						{
+							Console.Write("Пожалуйста, введите уникальный двузначный номер: ");
+							isInputValid = int.TryParse(Console.ReadLine(), out input) && input >= 10 && input <= 99 && !ArrayContainsValue(array, input);
+						}
+						array[i, j, k] = input;
+					}
+				}
+			}
+
+			Console.WriteLine("Трехмерный массив:");
+			for (int i = 0; i < 2; i++)
+			{
+				for (int j = 0; j < 2; j++)
+				{
+					for (int k = 0; k < 2; k++)
+					{
+						Console.Write($"{array[i, j, k]}({i},{j},{k}) ");
+					}
+					Console.WriteLine();
+				}
+			}
+		}
+
+		static bool ArrayContainsValue(int[,,] array, int value)
+		{
+			for (int i = 0; i < array.GetLength(0); i++)
+			{
+				for (int j = 0; j < array.GetLength(1); j++)
+				{
+					for (int k = 0; k < array.GetLength(2); k++)
+					{
+						if (array[i, j, k] == value)
+						{
+							return true;
+						}
+					}
+				}
+			}
+			return false;
+		}
+	}
+}
