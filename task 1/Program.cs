c6cbfbc664b266cc6085b01681b8032aa2e3fff8
malefikus13 <tverdokhlebov13@gmail.com>
2023-06-23 12:39:07 +0300

    lesson_9

---
+++ task 1/Program.cs
@@ -0,0 +1,27 @@
+﻿// Задача 64: Задайте значение N. Напишите программу, которая выведет все натуральные числа в промежутке от N до 1. Выполнить с помощью рекурсии.
+// N = 5 -> "5, 4, 3, 2, 1"
+// N = 8 -> "8, 7, 6, 5, 4, 3, 2, 1"
+
+using System;
+
+class Program
+{
+	static void Main(string[] args)
+	{
+		Console.Write("Введите число N: ");
+		int n = int.Parse(Console.ReadLine());
+
+		PrintNumbers(n); // вызов рекурсивной функции
+
+		Console.ReadLine();
+	}
+
+	static void PrintNumbers(int n)
+	{
+		if (n > 0) // условие выхода из рекурсии
+		{
+			Console.WriteLine(n); // вывод текущего числа
+			PrintNumbers(n - 1); // вызов функции с новым аргументом
+		}
+	}
+}
---
+++ task 3/Program.cs
@@ -0,0 +1,37 @@
+﻿// Задача 68: Напишите программу вычисления функции Аккермана с помощью рекурсии. Даны два неотрицательных числа m и n.
+// m = 2, n = 3 -> A(m,n) = 9
+// m = 3, n = 2 -> A(m,n) = 29
+
+using System;
+
+class Program
+{
+	static void Main(string[] args)
+	{
+		Console.Write("Введите значение M: ");
+		int m = int.Parse(Console.ReadLine());
+
+		Console.Write("Введите значение N: ");
+		int n = int.Parse(Console.ReadLine());
+
+		int result = Ackermann(m, n);
+
+		Console.WriteLine($"Результат функции Аккермана для M = {m} и N = {n} равен {result}");
+	}
+
+	static int Ackermann(int m, int n)
+	{
+		if (m == 0)
+		{
+			return n + 1;
+		}
+		else if (n == 0)
+		{
+			return Ackermann(m - 1, 1);
+		}
+		else
+		{
+			return Ackermann(m - 1, Ackermann(m, n - 1));
+		}
+	}
+}
